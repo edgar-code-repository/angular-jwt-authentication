0890477d447473b717fa3a429971e70933da34a5
Edgar <ecofre@gmail.com>
2024-03-14 23:12:16 -0300

    Routing was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +16,8 @@ import { LoginComponent } from './components/login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
