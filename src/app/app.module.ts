2ef9eff6adb3e3acd0a0e7e66c10efcd1c65f916
Edgar <ecofre@gmail.com>
2024-03-16 23:35:31 -0300

    FormsModule was imported, login template-driven form and form validation were added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -26,7 +27,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../model/user';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  blnLoginError: boolean = false;
+  errorMessage: string = "";
+
+  user: User = {
+    username: "",
+    password: ""
+  };  
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(submittedForm:any) {
+    //console.log(submittedForm);
+
+    if (submittedForm.invalid) {
+      return false;
+    }
+
+    this.user.username = submittedForm.value.loginEmail;
+    this.user.password = submittedForm.value.loginPassword;
+  
+    console.log(this.user.username);
+    console.log(this.user.password);
+
+    return true;
+  }
+
+  onClickBack() {
+    this.errorMessage = "";
+    this.blnLoginError = false;      
   }
 
 }
